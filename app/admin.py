cf3082a1798f9b48033fff394e57dd3004d41925
Niklas Lindman <Niklas.Lindman@student.careeria.fi>
2023-08-25 16:53:36 +0300

    Listaukset ja lisäykset toimii

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+# Jos rekisteröi tällä tavalla adminille oman appin
+# modelit, voi myös admin sivuilta muokata näitä tietoja.
 
-# Register your models here.
+from django.contrib import admin
+from app.models import Supplier, Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    pass
