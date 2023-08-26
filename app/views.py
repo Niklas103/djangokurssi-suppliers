845224cf3cf87a2712235317e5ca761403bec483
Niklas Lindman <Niklas.Lindman@student.careeria.fi>
2023-08-26 16:58:56 +0300

    2pvä malli koodit tehty

--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ def productlistview(request):
     context = {'products': productlist, 'suppliers': supplierlist}
     return render (request,"productlist.html",context)
 
+
 def addproduct(request):
     a = request.POST['productname']
     b = request.POST['packagesize']
@@ -21,11 +22,45 @@ def addproduct(request):
     Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
     return redirect(request.META['HTTP_REFERER'])
 
+
+def confirmdeleteproduct(request, id):
+    product = Product.objects.get(id = id)
+    context = {'product': product}
+    return render (request,"confirmdelprod.html",context)
+
+
+def deleteproduct(request, id):
+    Product.objects.get(id = id).delete()
+    return redirect(productlistview)
+
+
+def edit_product_get(request, id):
+        product = Product.objects.get(id = id)
+        context = {'product': product}
+        return render (request,"edit_product.html",context)
+
+
+def edit_product_post(request, id):
+        item = Product.objects.get(id = id)
+        item.unitprice = request.POST['unitprice']
+        item.unitsinstock = request.POST['unitsinstock']
+        item.save()
+        return redirect(productlistview)
+
+
+def products_filtered(request, id):
+    productlist = Product.objects.all()
+    filteredproducts = productlist.filter(supplier = id)
+    context = {'products': filteredproducts}
+    return render (request,"productlist.html",context)
+
+
 # Supplier view´s
 def supplierlistview(request):
     supplierlist = Supplier.objects.all()
     context = {'suppliers': supplierlist}
     return render (request,"supplierlist.html",context)
+
 
 def addsupplier(request):
     a = request.POST['companyname']
@@ -36,3 +71,10 @@ def addsupplier(request):
     f = request.POST['country']
     Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
     return redirect(request.META['HTTP_REFERER'])
+
+
+def searchsuppliers(request):
+    search = request.POST['search']
+    filtered = Supplier.objects.filter(companyname__icontains=search)
+    context = {'suppliers': filtered}
+    return render (request,"supplierlist.html",context)
